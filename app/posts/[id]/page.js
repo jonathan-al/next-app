5e110db1cbee0162e44849daeef2dc6695d02c05
Jonathan Alcantara Martinez <jonathan.informatics@gmail.com>
2025-06-10 16:28:36 -0600

    feat: update PostPage to await params and display post title and content

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,12 +1,26 @@
 import Link from "next/link";
+import posts from "@/app/data/posts";
 
-export default function PostPage({ params }) {
-  const { id } = params;
+export default async function PostPage({ params }) {
+  // Await params to fix the "params should be awaited" error
+  const { id } = await params;
+  const post = posts.find((post) => post.id === parseInt(id));
 
+  if (!post) {
+    return (
+      <main>
+        <h1>Post not found.</h1>
+        <p>
+          <Link href="/">Go back to Home Page</Link>
+        </p>
+      </main>
+    );
+
+  }
   return (
     <main>
-      <h1>Post ID: {id}</h1>
-      <p>This is the page for post with ID: {id}.</p>
+      <h1>Title: {post.title}</h1>
+      <p>Content: {post.content}</p>
       <p>
         <Link href="/">Go back to Home Page</Link>
       </p>
