90babcb96415f0ce4f58ec9d94e92e5d4cfd095e
Jonathan Alcantara Martinez <jonathan.informatics@gmail.com>
2025-06-11 10:26:39 -0600

    feat: add tailwind styling to the header on the home page

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to Our Application</h1>
+      <h1 className="text-3xl font-bold mb-6">Welcome to Our Application</h1>
       <p>This is the home page of our application.</p>
       <p>
         <Link href="/about">Go to About Page</Link>
